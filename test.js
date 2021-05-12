ff711fab06ca8735591bf3fec65e35403829bb1d
younheehwan <moohwan@kiswire.com>
2021-05-12 17:46:05 +0900

    몽고DB Find

---
+++ test.js
@@ -0,0 +1,28 @@
+var user = {
+    id: 5,
+    username: "min"
+}
+
+var userList = [
+    {
+        id: 1,
+        username: "ssar",
+    },
+    {
+        id: 2,
+        username: "cos"
+    },
+    {
+        id: 3,
+        username: "love"
+    },
+    {
+        id: 4,
+        username: "hong"
+    },
+    {
+        id: 5,
+        username: "choi",
+        friend:2
+    }
+]
